b67c631075aada3bc6a63bafdbbbe6c464011791
Davi Paiva <50916238+paivadavi@users.noreply.github.com>
2022-05-13 17:54:51 +0200

    Added description to the problem

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,14 @@
+#Problem description
+#Print numbers from 1 to 100 inclusively following these instructions:
+#
+#    if a number is multiple of 3, print "Fizz" instead of this number
+#    if a number is multiple of 5, print "Buzz" instead of this number
+#    for numbers that are multiples of both 3 and 5, print "FizzBuzz"
+#    print the rest of the numbers unchanged.
+#
+#Important: This problem was proposed to me as a challenge during my studies in JetBrains Academy
+
+
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
