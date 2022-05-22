91d0e2cf5b4898ba2395826a680c1432b663eb4e
Davi Paiva <50916238+paivadavi@users.noreply.github.com>
2022-05-22 19:22:54 +0200

    First addition of the 'ten_files.py' file
    A FIle containing a simple procedure to open 10 files with the assistance of a context manager in Python.

---
+++ ten_files.py
@@ -0,0 +1,10 @@
+#Create 10 files, named file1.txt, file2.txt and so on till file10.txt. The files should contain the number corresponding to their name. 
+#So, file1.txt should contain one line with number 1, file2.txt — one line with number 2 and so on.
+
+file_template = "file"
+for i in range(1, 11):
+    file_name = "{}{}{}".format("file", str(i), ".txt")
+    with open (file_name,'w') as current_file:
+        current_file.write(str(i))
+       
+#Problem encountered on JetBrainsAcademy
